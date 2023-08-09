b9cb8e566c721f5670d821ad354d7c0d31c258a5
Tanuj0202 <140401828+Tanuj0202@users.noreply.github.com>
2023-08-09 12:30:19 +0530

    +-,scanf,%d

---
+++ jecrcclass2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //sjdklfsdf sodfjskdjfsleifdk
+    int a=23;
+    int b=65;
+
+    printf("The sum of a and b is:%d\n",a+b);
+    printf("Hello World\n");
+    printf("My name is\t");
+    printf("Tanuj Methi\n");
+
+    int c,d;
+
+    printf("Enter the first number:");
+    scanf("%d",&c);
+    printf("Enter the second number:");
+    scanf("%d",&d);
+    printf("The sum of c and d is:%d\n",c+d);
+
+    int e,f;
+    scanf("%d%d",&e,&f);
+    printf("e+f=%d\n",e+f);
+
+   printf("mera\nsdjfiosd\n sdjhf");
+
+    return 0;
+}
