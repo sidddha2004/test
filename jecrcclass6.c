1f56bd3fe3d3c915d0d53574ac2d04bf62697874
Tanuj0202 <140401828+Tanuj0202@users.noreply.github.com>
2023-09-13 12:35:15 +0530

    Decision control/ Conditional statement
    Decision control/ Conditional statement:-
    1. If    2. If-else    3. If-else-If
    
    Created vote program

---
+++ jecrcclass6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    printf("Decision control/ Conditional statement:-\n");
+    printf("1. IF \t 2. If-else \t 3. If-else-If\n");
+    printf("Using If-else\n");
+
+    int a;
+    printf("Enter your age:-");
+    scanf("%d",&a);
+    if (a>18){
+    printf("You are Eligible to give Vote");
+    }
+    else {
+        printf("You are NOT Eligible to give Vote");
+    }
+
+
+
+
+
+    return 5;
+
+}
